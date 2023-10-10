8b76c28cce45d644c5017cf655942a03216cc7ff
Drecksha <smashava91@gmail.com>
2023-10-09 19:57:42 -0700

    101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ *main - print 3 digits
+ *Return: Always 0
+ */
+
+int main(void)
+
+{
+	int i = '0';
+	int j;
+	int k;
+	while (i <= '9')
+        {
+		j = '0';
+		while (j <= '9')
+		{
+		k ='0';
+		while(k <= '9')
+		{
+		    		if (i < j && j < k)
+			{
+				putchar(i);
+				putchar(j);
+				putchar(k);
+       				if (i ! = 7)
+
+				{
+				putchar(',')
+				putchar(' ');
+				}
+			}
+			k++;
+		}
+		j++
+	}
+	i++
+	putchar('\n');
+	return (0);
+}
