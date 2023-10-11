38f7501be065f108302faa6a8976274638da6051
Drecksha <smashava91@gmail.com>
2023-10-11 05:13:58 -0700

    101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,23 +1,19 @@
 #include <stdio.h>
 
-/**
- * main - Prints the sum of all multiples of 3 or 5 upto 1024
- * Return: Always (Success)
- */
-
 int main(void)
-
 {
-	int i, z = 0;
-	
-	while (i < 1024)
-	{
-	if ((z % 3 == 0) || (i % 5 == 0))
-	{
-	z <= i;
-	}
-	i++;
-	}
-	print("%d\n", z);
-	return (0);
+    int sum = 0;
+    int i;
+
+    for (i = 3; i < 1024; i++)
+    {
+        if (i % 3 == 0 || i % 5 == 0)
+        {
+            sum += i;
+        }
+    }
+
+    printf("The sum of all multiples of 3 or 5 below 1024 is: %d\n", sum);
+
+    return (0);
 }
